d39d32d645adcc4cb7c95e02cf69b2d00f4a1b1c
CarlosBerges <110702660+CarlosBerges@users.noreply.github.com>
2022-11-01 17:33:48 -0300

    .

---
+++ java.js
@@ -0,0 +1,15 @@
+let nombreUsuario=prompt("Ingrese su nombre:")
+alert("NOMBRE:"+nombreUsuario)
+let edad = parseFloat(prompt("Ingrese su edad: "))
+function condicional(parametro){
+    if(parametro > 18){ 
+       alert("Puede ingresar")
+    }else{
+       alert("No puede ingresar")
+    }
+   }
+   
+   condicional(edad)
+
+'/con el java voy a hacer botones para los productos y agregarlos al carrito, y justamente el carrito./'
+
